45d1a3a1555d36d4e3f9bd792653d66eced9a5b0
bmuftuoglu <79010159+bmuftuoglu@users.noreply.github.com>
2023-05-21 00:27:26 +0300

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,91 @@
+<div class="cv-col1">
+    <h2 class="cv-col1-title"><b>CV</b></h2>
+    <div class="yellow-box"></div>
+</div>
+<div class="cv-col2">
+    <div class="cv-col2-container">
+        <h2 style="font-size: 40px;"><span style="font-weight: normal;">Professional</span> Summary</h2>
+        <div class="cv-col2-content">
+            I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double
+            click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you
+            like on your page. I’m a great place for you to tell a story and let your users know a little more about
+            you. <br><br><br>
+
+            ​
+
+            This is a great space to write a long text about your company and your services. You can use this space to
+            go into a little more detail about your company. Talk about your team and what services you provide. Tell
+            your visitors the story of how you came up with the idea for your business and what makes you different from
+            your competitors. Make your company stand out and show your visitors who you are.
+        </div>
+        <a href="cv.pdf" style="color: black;">
+            <div class="cv-col2-button">
+                Download Full CV
+            </div>
+        </a>
+    </div>
+</div>
+<div class="cv-col3">
+    <h2 class="cv-col3-title"><b>Skills</b></h2>
+    <div class="yellow-box"></div>
+</div>
+<div class="cv-col4">
+    <div class="cv-col4-container">
+        <p style="text-align: center; font-size: 20px; line-height: 2em;font-weight: bold;">
+            <span class="skill-bg">HTML</span>
+            <span class="skill-bg">CSS</span>
+            <span class="skill-bg">Javascript</span>
+            <span class="skill-bg">Python</span>
+            <span class="skill-bg">Flutter</span>
+            <span class="skill-bg">Dart</span><br>
+            <span class="skill-bg">MySQL</span>
+            <span class="skill-bg">MongoDB</span>
+            <span class="skill-bg">Django</span>
+            <span class="skill-bg">FireBase</span>
+            <span class="skill-bg">C#</span>
+            <span class="skill-bg">Java</span>
+        </p>
+    </div>
+</div>
+<div class="cv-col5">
+    <h2 class="cv-col5-title">Work <b>Experience</b></h2>
+    <div class="yellow-box"></div>
+</div>
+<div class="cv-col6">
+    <div class="cv-col6-container column">
+        <div class="job">
+            <p>2015 - 2016</p>
+            <h3 style="margin: 5px 0;">ARMOYU - Full Stack Developer</h3>
+            <p>I worked as a full stack developer at Aramizdaki Oyuncu Gaming Community.</p>
+        </div>
+        <div class="job">
+            <p>2016 - 2020</p>
+            <h3 style="margin: 5px 0;">ARMOYU - C# Developer</h3>
+            <p>I worked as a C# developer at Aramizdaki Oyuncu Gaming Community.</p>
+        </div>
+        <div class="job">
+            <p>2020 - Continues</p>
+            <h3 style="margin: 5px 0;">ARMOYU - Mobile App Developer</h3>
+            <p>I have been working as an Android and IOS (cross platform) developer by using Flutter at
+                Aramizdaki Oyuncu Gaming Community.</p>
+        </div>
+    </div>
+</div>
+<div class="cv-col7">
+    <h2 class="cv-col7-title"><b>Education</b></h2>
+    <div class="yellow-box"></div>
+</div>
+<div class="cv-col8">
+    <div class="cv-col8-container column">
+        <div class="education">
+            <p>2020 - 2026</p>
+            <h3 style="margin: 5px 0;">Middle East Technical University</h3>
+            <p>Computer Education and Instructional Technology</p>
+        </div>
+        <div class="education">
+            <p>2021 - 2023</p>
+            <h3 style="margin: 5px 0;">Atatürk University</h3>
+            <p>Computer Programming</p>
+        </div>
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,40 @@
+<header class="header">
+        <div class="header-left header-width">
+            <div class="name-infos">
+                <p>
+                    <a href="/" class="header-name-title">
+                        I'm Barış Müftüoğlu
+                    </a>
+                </p>
+                <p>
+                    A Mobile and Full Stack Developer
+                </p>
+            </div>
+        </div>
+        <div class="header-right header-width">
+            <ul class="nav-links">
+                <a href="/">
+                    <li class="nav-links-items">Home</li>
+                </a>
+                <a href="?page=cv">
+                    <li class="nav-links-items">CV</li>
+                </a>
+                <a href="?page=contact">
+                    <li class="nav-links-items">Contact</li>
+                </a>
+            </ul>
+            <ul class="social-media-links">
+                <li><a href="https://www.instagram.com/iamnotmuftuoglu/" target="_blank"><img src="images/instagram.png"
+                            alt="Instagram" width="20px" height="20px"></a></li>
+                <li><a href="https://twitter.com/xmuftuoglu" target="_blank"><img src="images/twitter.png" alt="Twitter"
+                            width="20px" height="20px"></a></li>
+                <li><a href="https://www.linkedin.com/in/bar%C4%B1%C5%9Fm%C3%BCft%C3%BCo%C4%9Flu/" target="_blank"><img
+                            src="images/linkedin.png" alt="LinkedIn" width="20px" height="20px"></a></li>
+                <li><a href="https://vsco.co/iamnotmuftuoglu/gallery" target="_blank"><img src="images/vsco.png"
+                            alt="Vsco" width="20px" height="20px"></a></li>
+                <li><a href="https://github.com/bmuftuoglu" target="_blank"><img src="images/github.png" alt="Vsco"
+                            width="20px" height="20px"></a></li>
+            </ul>
+        </div>
+
+    </header>
